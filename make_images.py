3743d383a1f26ceffe606029515bc11dc6dde9d7
Nathan Shepherd <nateshep@umich.edu>
2018-06-02 22:05:18 -0400

    Updates

--- make_images.py
+++ make_images.py
@@ -8,22 +8,25 @@ import os
 output_size = (28, 28)
 data_dir = 'images/pokemon/'
 
-if __name__ == "__main__":
-
+def down_sample(data_dir):
   print('Downsampling images')
+
   for i, filename in enumerate(os.listdir(data_dir)):
-      if i % len(os.listdir(data_dir)) / 100 == 0:
+      if i % 50 == 0:
           print(i*100/(len(os.listdir(data_dir))))
           
       img = Image.open(data_dir + filename)
       filename, f_ext = os.path.splitext(filename)
 
       # Downsample image size
-      img.resize(output_size, Image.ANTIALIAS)
+      img.thumbnail(output_size, Image.ANTIALIAS)
       if output_size[0] == output_size[1]:
           str_size = str(output_size[0])
           
           img.save('%s/%s/~%s_%s%s' % (data_dir[:-1], str_size,
                                        filename, str_size, f_ext))
-
   print('Downsampling complete!')
+
+if __name__ == "__main__":
+    down_sample(data_dir)
+  
--- vae.py
+++ vae.py
@@ -4,6 +4,7 @@
 # Developed by Nathan Shepherd
 
 import os
+import time
 import numpy as np
 from PIL import Image
 import tensorflow as tf
@@ -13,28 +14,145 @@ import matplotlib.pyplot as plt
 #mnist = input_data.read_data_sets('/tmp/data/', one_hot=True)
 
 
-
-
-class VAE:
+class NeuralNetwork:
     def __init__(self):
         pass
+    
+    def define_input(self):
+        return tf.placeholder(tf.float32, shape=([None, self.img_pixels]))
 
-    def load_dataset(self, data_dir):
-        print('Loading dataset'); img_data = []
+    def perceptron(self, x, w, b):
+        return tf.matmul(x, w) + b
+
+    def weight_variable(self, shape, name):
+        initial = tf.truncated_normal(shape, stddev=0.1)
+        return tf.Variable(initial, name=name)
+
+    def bias_variable(self, shape, name):
+        initial = tf.truncated_normal(shape, stddev=0.1)
+        return tf.Variable(initial, name=name)
+
+    def load_dataset(self, data_dir, img_pixels=28*28):
+        img_data = []
+        print('Loading dataset')
+        self.img_pixels = img_pixels
         for i, filename in enumerate(os.listdir(data_dir)):
-          if (i % (len(os.listdir(data_dir)) / 10)) == 0:
+          if i % (50) == 0:
               print(i*100/(len(os.listdir(data_dir))))
 
           img = Image.open(data_dir + filename)
           img_data.append(img)
 
         self.img_data = img_data
+        print('Loaded dataset successfully')
 
+class VAE(NeuralNetwork):
+    def __init__(self, latent_dim, hidden):
+        self.latent_dim = latent_dim
+        self.img_pixels = IMG_PIXELS
+        self.hidden = hidden
 
+        # Build the model: Variational AutoEncoder
+        self.X = self.define_input()
+        self.define_encoder()
+        self.define_decoder()
+        self.compile_model()
+
+    def train(self, epochs, batch_size=10):
+        init = tf.global_variables_initializer()
+        sess = tf.InteractiveSession()
+        saver = tf.train.Saver()
+        sess.run(init)
+        
+        variational_lower_bound_array = []
+        log_likelihood_array = []
+        KL_term_array = []
+        
+        for i in range(0, epochs - batch_size, batch_size):
+            x_batch = [self.PIL2array(pic) for pic in self.img_data[i: i + batch_size]]
+
+            sess.run(self.optimizer, feed_dict={self.X: x_batch})
+
+            if i % 10 == 0:
+                vlb_eval = self.variational_lower_bound.eval(feed_dict={self.X: x_batch})
+                print("Iteration: {}, Loss: {}".format(i, vlb_eval), end="")
+                variational_lower_bound_array.append(vlb_eval)
+                #log_likelihood_array.append(np.mean(log_likelihood.eval(feed_dict={X: x_batch})))
+                #KL_term_array.append(np.mean(KL_term.eval(feed_dict={X: x_batch})))
+
+                
+
+                
+                
+            
+            
+
+    def compile_model(self):
+        #Information is lost because it goes from a smaller to a larger dimensionality. 
+        #How much information is lost? We measure this using the reconstruction log-likelihood 
+        #This measure tells us how effectively the decoder has learned to reconstruct
+        #an input image x given its latent representation z.
+        log_likelihood = tf.reduce_sum(self.X * tf.log(self.reconstruction + 1e-9)\
+                                       + (1 - self.X)*tf.log(1 - self.reconstruction + 1e-9),
+                                       reduction_indices = 1)
+
+        KL_div = -.5 * tf.reduce_sum(1 + 2*self.logstd\
+                                            - tf.pow(self.mu, 2)\
+                                            - tf.exp(2 * self.logstd),
+                                            reduction_indices = 1)
+
+        # This allows us to use stochastic gradient descent with respect to the variational parameters
+        self.variational_lower_bound = tf.reduce_mean(log_likelihood - KL_div)
+        self.optimizer = tf.train.AdadeltaOptimizer().minimize(-self.variational_lower_bound)
+        
+
+    def generate(self, seed):
+        if seed == None: pass
+
+    def define_encoder(self):
+        # First layer is a simple feed forward network
+        w_enc = self.weight_variable([self.img_pixels, self.latent_dim], 'W_encoder')
+        b_enc = self.bias_variable([self.latent_dim], 'B_encoder')
+        h_enc = tf.nn.tanh(self.perceptron(self.X, w_enc, b_enc))
+
+        # Mean
+        w_mu = self.weight_variable([self.latent_dim, self.hidden[0]], 'W_mu')
+        b_mu = self.bias_variable([self.hidden[0]], 'B_mu')
+        self.mu = self.perceptron(h_enc, w_mu, b_mu)
+
+        # Standard Deviation
+        w_logstd = self.weight_variable([self.latent_dim, self.hidden[0]], 'W_logstd')
+        b_logstd = self.bias_variable([self.hidden[0]], 'B_logstd')
+        self.logstd = self.perceptron(h_enc, w_logstd, b_logstd)
+
+        noise = tf.random_normal([1, self.latent_dim])
+        
+        # Z is the output parameter of encoder
+        self.Z = self.mu + tf.matmul(noise, tf.exp(.5*self.logstd))
+
+    def define_decoder(self):
+        w_dec = self.weight_variable([self.hidden[-1], self.latent_dim], 'W_decoder')
+        b_dec = self.bias_variable([self.latent_dim], 'V_decoder')
+        h_dec = tf.nn.tanh(self.perceptron( self.Z, # Output from encoder
+                                            w_dec, b_dec))
+
+        w_reconstruct = self.weight_variable([self.latent_dim, self.img_pixels], 'W_reconstruct')
+        b_reconstruct = self.bias_variable([self.img_pixels], 'B_reconstruct')
+        self.reconstruction = tf.nn.sigmoid(self.perceptron(h_dec, w_reconstruct, b_reconstruct))
+
+    def PIL2array(self, img):
+        return np.array(img.getdata(),
+                    np.uint8).reshape(img.size[1], img.size[0], 3)
+        
+        
+
+        
+IMG_PIXELS = 28*28       
 
 
 if __name__ == "__main__":
-    vae = VAE()
+    vae = VAE(500, [20])
     vae.load_dataset('images/pokemon/28/')
+    vae.train(100)
     
     
