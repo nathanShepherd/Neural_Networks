f96bbbf00e0375852916020cacae745dd82a7a33
Nathan Shepherd <nateshep@umich.edu>
2018-06-13 23:07:09 -0400

    Update vae_source_bluebelmont.py

--- vae_source_bluebelmont.py
+++ vae_source_bluebelmont.py
@@ -1,3 +1,5 @@
 # bluebelmont/Variational-Autoencoder
 # https://github.com/bluebelmont/Variational-Autoencoder/blob/master/Variational%20Autoencoder.ipynb
-
+#
+# Related work: Optimizing the Latent Space of Generative Network
+# https://arxiv.org/pdf/1707.05776.pdf
